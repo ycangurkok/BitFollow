002f3683d1c56d735c73ff70ae954e9abfa775ea
ycangurkok <ycangurkok@gmail.com>
2020-06-07 14:51:02 +0300

    Major changes to code
    Will show color based on price movement compared to the previous return. If the price has increased, will turn green, and vice versa. If the price remains as is, will keep the same color as previous return. Also added a net difference counter, which compares the returned price to the first returned price when the program is launched in order to keep track of movement within the session.

--- BitFollow.py
+++ BitFollow.py
@@ -1,13 +1,38 @@
 from urllib.request import urlopen
 import time
+from termcolor import colored
 
-def pricecalc():
+cval = 0
+color = ""
+startprice = 0
+
+def getstartprice():
+    global startprice
     response = urlopen('https://blockchain.info/tobtc?currency=TRY&value=100')
     html_doc = response.read()
     html_doc = html_doc.decode('utf-8')
-    price = 100/(float(html_doc))
-    print(int(price))
+    startprice = int(100/(float(html_doc)))
 
+def pricecalc():
+    global cval, color
+    response = urlopen('https://blockchain.info/tobtc?currency=TRY&value=100')
+    html_doc = response.read()
+    html_doc = html_doc.decode('utf-8')
+    price = int(100/(float(html_doc)))
+    nval = price
+    if(nval > cval):
+        cval = nval
+        color = "green"
+        print(colored(price, color) , "Net difference of" , (nval - startprice))
+    elif(nval == cval):
+        cval = nval
+        print(colored(price, color) , "Net difference of" , (nval - startprice))
+    else:
+        cval = nval
+        color = "red"
+        print(colored(price, color) , "Net difference of" , (nval - startprice))
+
+getstartprice()
 while(True):
     pricecalc()
-    time.sleep(5)
+    time.sleep(30)
